478caad3dc911a0b6d0b94fa0c29210e46b41213
Tarinaroope <alvi.hirvela@gmail.com>
2025-02-24 21:41:06 +0200

    Code cleanup

--- src/main.c
+++ src/main.c
@@ -18,19 +18,13 @@
 #include "rf_pico.h"
 #include "protocol.h"
 
+#define POWER_LED_INDICATOR_GPIO 20
+
 #define TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
-
-const char *WIFISSID = WIFI_SSID;
-const char *WIFIPASSWORD = WIFI_PASSWORD;
-const char *MQTTHOST = MQTT_HOST;
-const int MQTTPORT = MQTT_PORT;
-const char *MQTTUSER = MQTT_USER;
-const char *MQTTPASSWD = MQTT_PASSWD;
-const char *TOPICROOT = "SENSOR";
-const char *TOPICTEMPERATURE = "TEMPERATURE";
-
 #define MAX_SENSORS 10
 #define UPDATE_INTERVAL 5000 //ms
+#define ROOT_TOPIC "SENSOR"
+#define TEMPERATURE_TOPIC "TEMPERATURE"
 
 uint32_t sensors[MAX_SENSORS];
 
@@ -41,6 +35,7 @@ typedef struct
 } queue_entry_t;
 
 queue_t message_queue;
+queue_t status_queue;
 
 bool needs_update(uint8_t sensorId)
 {
@@ -60,73 +55,77 @@ bool needs_update(uint8_t sensorId)
     }
 }
 
+// pmicro-rf callback in case of incoming sensor information
 void report_result(RF_Message* message)
 {
+    // Get the sensor where the message is from
     uint8_t sensorId = get_device_address(&message->message);
     if (needs_update(sensorId))
     {
         queue_entry_t entry;
         entry.sensorId = sensorId;
+        // Now we only get temperature
         entry.value = get_temperature(&message->message);
         queue_try_add(&message_queue, &entry);
     }
+}
+
+// Mqttthing callback for online status
+void connectCallback(bool online)
+{
+    if (queue_is_full(&status_queue))
+    {
+        queue_try_remove(&status_queue, NULL);
+    }
+    queue_try_add(&status_queue, &online);
 }
 
 void main_task(void *params)
 {
     MQTTThing thing;
 
-     // Setup for MQTT Connection
-    char mqttTarget[] = MQTT_HOST;
-    int mqttPort = MQTT_PORT;
-    char mqttClient[] = MQTT_CLIENT;
-    char mqttUser[] = MQTT_USER;
-    char mqttPwd[] = MQTT_PASSWD;
-
-    if (!mqttthing_init(&thing, WIFISSID, WIFIPASSWORD, MQTTHOST, MQTTPORT, MQTTUSER, MQTTPASSWD))
+    // Create buffers
+    char* topicBuffer = pvPortMalloc(strlen(TEMPERATURE_TOPIC) + 5);
+    char* payloadBuffer = pvPortMalloc(8);
+    if (topicBuffer == NULL || payloadBuffer == NULL ||
+        (!mqttthing_init(&thing, WIFI_SSID, WIFI_PASSWORD, MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASSWD, ROOT_TOPIC)) )
     {
-        LogError(("Failed to initialize MQTTThing\n"));
+        vPortFree(topicBuffer);
+        vPortFree(payloadBuffer);
+        LogError(("Failed to initialize/allocate memory!\n"));
         return;
     }
 
-    mqttthing_connectLoop(&thing);
+    mqttthing_connectLoop(&thing, connectCallback);
 
-    char topicBuffer[40];
-    char payloadBuffer[10];
     queue_entry_t entry = {0};
+
     while (true)
     {
-   
-        if (queue_try_remove(&message_queue, &entry))
+        while(queue_try_remove(&message_queue, &entry))
         {
-            sprintf(topicBuffer, "%s/%s/%d", TOPICROOT, TOPICTEMPERATURE, entry.sensorId);
+            sprintf(topicBuffer, "%d/%s", entry.sensorId, TEMPERATURE_TOPIC );
             sprintf(payloadBuffer, "%3.2f", entry.value);
             printf("Publishing to %s, payload %s\n", topicBuffer, payloadBuffer);
             mqttthing_publish(&thing, topicBuffer, payloadBuffer);
         }
-        vTaskDelay(pdTICKS_TO_MS(1000U));
-        
+        vTaskDelay(pdTICKS_TO_MS(2000U));
     }
+
+    vPortFree(topicBuffer);
+    vPortFree(payloadBuffer);
 }
 
 void vLaunch(void)
 {
-    TaskHandle_t task;
-
-    xTaskCreate(main_task, "MainThread", 512, NULL, TASK_PRIORITY, &task);
-
-    /* Start the tasks and timer running. */
+    xTaskCreate(main_task, "MainThread", 512, NULL, TASK_PRIORITY, NULL);
     vTaskStartScheduler();
 }
 
 void core1_main(void)
 {
-    /* Configure the hardware ready to run the demo. */
-    const char *rtos_name;
-    rtos_name = "FreeRTOS";
-    printf("Starting %s on core 1:\n", rtos_name);
+    printf("Starting FreeRTOS on core %d...\n", get_core_num());
     sleep_ms(1000);
-
     vLaunch();
 }
 
@@ -137,25 +136,39 @@ int main(void)
 
     sleep_ms(1000);
 
-    printf("Init...\n");
+    printf("SensorHub init...\n");
+
+    // Setup power led indicator
+    gpio_init(POWER_LED_INDICATOR_GPIO);
+    gpio_set_dir(POWER_LED_INDICATOR_GPIO, GPIO_OUT);
     
     queue_init(&message_queue, sizeof(queue_entry_t), 20);
+    queue_init(&status_queue, sizeof(bool), 1);
     memset(sensors, 0, sizeof(sensors));    
-
     multicore_launch_core1(core1_main);
 
-    printf("Waiting...");
-    sleep_ms(20000);
-
-    rf_pico_receiver rec;
-    pico_init_receiver(&rec, report_result);
-    sleep_ms(1000);
-    pico_rx_start_receiving(&rec);
-
-
-    while (1)
+    printf("Initializing RF receiver system on core %d. Waiting for MQTT system to start.\n", get_core_num());
+    bool mqttOnline = false;
+    bool ledState = true;
+    while (!queue_try_peek(&status_queue, &mqttOnline) || !mqttOnline)
     {
-       sleep_ms(1000);
+        // Indicate the initialization process with flashing power led
+        gpio_put(POWER_LED_INDICATOR_GPIO, ledState);
+        sleep_ms(1000);    
+        ledState = !ledState;
+    }
+  
+    printf("MQTT system is online. Starting RF receiver system.\n");
+
+    rf_pico_receiver receiver;
+    pico_init_receiver(&receiver, report_result);
+    pico_rx_start_receiving(&receiver);
+    ledState = true;
+    while (true)
+    {
+        gpio_put(POWER_LED_INDICATOR_GPIO, ledState);
+        sleep_ms(3000); 
+        ledState = !ledState;
     }
     return 0;
 }
